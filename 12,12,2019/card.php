cddb48df1a53f7a0a33fe97846ae2c64c08ccca7
KalolinaJ <43820866+KalolinaJ@users.noreply.github.com>
2019-09-12 14:40:54 +0200

    Add files via upload

---
+++ 12,12,2019/card.php
@@ -0,0 +1,26 @@
+<?php
+echo('
+                <div class="card">
+                        <div class="images">1
+                            <img class="photo" src="https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fpictures.topspeed.com%2FIMG%2Fjpg%2F201608%2F2014-ferrari-laferrari-26.jpg&f=1&nofb=1" alt="ferrari">
+                            
+                                <p class="price">cena: <span class="price-value">123 000 000 zł</span></p>
+                                <span class="promo"><span class="prom">promocja!</span></span>
+                                <div class="color">Kolor:
+                                    <ul class="list">
+                                        <li class="size-item1">Czerwony</li>
+                                        <li class="size-item2">Zielony</li>
+                                        <li class="size-item3">Czarny</li>
+                                    </ul>
+                            
+                                </div>
+                        </div>
+                <div class="desc">
+                    <h2 class="title">Ferrari</h2>
+                    <p class="description">Czerwony supersamochód, osiąga zawrotne prędkości w krótkim czasie</p>
+                    <a href="#">więcej</a>
+                    <a href="#">kup teraz</a>
+                    </div>
+                </div>
+')
+?>
---
+++ 12,12,2019/index.php
@@ -0,0 +1,22 @@
+  
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" /> 
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+</head>
+<body>
+    <div class="container">
+        <div class="header">header</div>
+        <div class="sidebar">sidebar</div>
+        
+        <?php
+            require("card.php")
+        ?>
+
+        <div class="footer">footer</div>
+
+    </div>
+</body>
+</html>
